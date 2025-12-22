043310a139afe0fa0e36be7bd8e16b6db54dde4f
Antoine Griffard <agriffard@hotmail.com>
2025-12-22 15:08:20 +0100

    NatoAlphabet

--- src/NatoAlphabet/NatoConverter.cs
+++ src/NatoAlphabet/NatoConverter.cs
@@ -9,34 +9,34 @@ public static class NatoConverter
     public static Dictionary<char, string> LetterToPhonetic()
     {
         return new Dictionary<char, string>(CharComparer.OrdinalIgnoreCase)
-            {
-                {'A', "Alpha"},
-                {'B', "Bravo"},
-                {'C', "Charlie"},
-                {'D', "Delta"},
-                {'E', "Echo"},
-                {'F', "Foxtrot"},
-                {'G', "Golf"},
-                {'H', "Hotel"},
-                {'I', "India"},
-                {'J', "Juliett"},
-                {'K', "Kilo"},
-                {'L', "Lima"},
-                {'M', "Mike"},
-                {'N', "November"},
-                {'O', "Oscar"},
-                {'P', "Papa"},
-                {'Q', "Quebec"},
-                {'R', "Romeo"},
-                {'S', "Sierra"},
-                {'T', "Tango"},
-                {'U', "Uniform"},
-                {'V', "Victor"},
-                {'W', "Whiskey"},
-                {'X', "X-ray"},
-                {'Y', "Yankee"},
-                {'Z', "Zulu"}
-            };
+        {
+            {'A', "Alpha"},
+            {'B', "Bravo"},
+            {'C', "Charlie"},
+            {'D', "Delta"},
+            {'E', "Echo"},
+            {'F', "Foxtrot"},
+            {'G', "Golf"},
+            {'H', "Hotel"},
+            {'I', "India"},
+            {'J', "Juliett"},
+            {'K', "Kilo"},
+            {'L', "Lima"},
+            {'M', "Mike"},
+            {'N', "November"},
+            {'O', "Oscar"},
+            {'P', "Papa"},
+            {'Q', "Quebec"},
+            {'R', "Romeo"},
+            {'S', "Sierra"},
+            {'T', "Tango"},
+            {'U', "Uniform"},
+            {'V', "Victor"},
+            {'W', "Whiskey"},
+            {'X', "X-ray"},
+            {'Y', "Yankee"},
+            {'Z', "Zulu"}
+        };
     }
 
     // NATO phonetic word → Letter
